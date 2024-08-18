dbaff46a29cd63030aa0fcf68f3c4fe7ed616db5
mohsin <mohsin.ahmad7676@gmail.com>
2024-08-19 00:09:56 +0500

    Initialize Project

---
+++ InventoryPOS/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryPOS.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
